b0350b2f6175a5dd937e706d1412b15626782aa8
GESPIJ <gregspinetti@gmail.com>
2020-12-24 03:08:22 +0100

    first

--- segundo.py
+++
@@ -1,9 +0,0 @@
-# from pip._internal import main as install
-# try:
-#     import cv2
-# except ImportError as e:
-#     install(["install", "opencv-python"])
-# finally:
-#     pass
-import tensorflow as tf
-print("Hola")
