1740c0f4866ca944d14d386da312bc5ff5dfb0fc
GESPIJ <gregspinetti@gmail.com>
2020-12-26 00:28:38 +0100

    second

--- traffic.py
+++ traffic.py
@@ -16,13 +16,13 @@ TEST_SIZE = 0.4
 def main():
 
     # Check command-line arguments
-    #if len(sys.argv) not in [2, 3]:
-    #    sys.exit("Usage: python traffic.py data_directory [model.h5]")
+    if len(sys.argv) not in [2, 3]:
+       sys.exit("Usage: python traffic.py data_directory [model.h5]")
 
     # Get image arrays and labels for all image files
-    #images, labels = load_data(sys.argv[1])
-    print("Hola")
-    #images, labels = load_data('gtsrb')
+    images, labels = load_data(sys.argv[1])
+    #print("Hola")
+    images, labels = load_data('gtsrb')
 
     # Split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
@@ -49,23 +49,27 @@ def main():
 def load_data(data_dir):
     """
     Load image data from directory `data_dir`.
-
     Assume `data_dir` has one directory named after each category, numbered
     0 through NUM_CATEGORIES - 1. Inside each category directory will be some
     number of image files.
-
     Return tuple `(images, labels)`. `images` should be a list of all
     of the images in the data directory, where each image is formatted as a
     numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
+    images = []
+    labels = []
+    for folder in os.listdir(data_dir):
+        folder_path = os.path.join(data_dir, folder)
+        if os.path.isdir(folder_path):
+            for image_file in os.listdir(folder_path):
+                image = cv2.imread(os.path.join(folder_path, image_file), cv2.IMREAD_COLOR)
+                image = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_AREA)
+                images.append(image)
+                labels.append(int(folder))
 
-    for number in range(43):
-        if (os.path.isdir(os.path.join(data_dir,str(number)))):
-            print(number)
-
-    raise NotImplementedError
+    return images, labels
 
 
 def get_model():
